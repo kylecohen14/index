c95f50f7b579789fe40d9d9f7b32d1f72627f04c
kylec <KYLECOHEN14@GMAIL.COM>
2021-05-27 15:53:24 -0400

    deleted 2.js

--- app.js
+++ app.js
@@ -6,18 +6,40 @@
 // console.log(username)
 
 
+function getUserName(){
+    let userName = prompt('Enter Name')
+    console.log(userName);
+    document.write('<h2>' + userName + '</h2>');
+    return userName;
+    
+}
 
-let userName = prompt('Enter Name: ');
-console.log(userName);
-document.write('<h2>' + userName + '</h2>');
+// let user1 = getUserName
+// console.log(user1);
 
-
-
-let question1 = prompt('Do you love fishing?');
+function getUserAnwser(){
+    let question1 = prompt('Do you love fishing? (yes or no)');
 console.log(question1);
 
-if (userAnswer == 'yes') {
-    document.write('<h3>Meet me at the boat ramp! ' + userName +'</h3>');
+if (question1.toLowerCase() == 'yes') {
+    document.write('<h3>Meet me at the boat ramp!</h3>');
 } else {
-    document.write('<h3>' + userName +', go buy some fishing gear.</h3>');
+    document.write('<h3>Lets go buy some fishing gear.</h3>');
+}
+}
+function getUserAnwser(){
+    let question2 = prompt('have you been fishing?')
+    let url ="https://pbs.twimg.com/profile_images/605189676476039168/5qZTHZK__400x400.png"
+    if (question2.toLowerCase() == 'yes'){
+        document.write('<h3>Good for you!</h3>');
+        document.write('<img src="https://pbs.twimg.com/profile_images/605189676476039168/5qZTHZK__400x400.png"/>');
+    }
+}
+let user1 = getUserAnwser();
+
+let likeFishing = prompt('Ready to go fishing?');
+
+if (likeFishing == 'yes'){
+    document.write('<h3>You have your priorities right</h3>');
+    document.write('<img src="https://content.osgnetworks.tv/gameandfishing/content/photos/gaf-12Punch-SMB.jpg"/>');
 }
--- app2.js
+++
@@ -1,6 +0,0 @@
-let likeFishing = prompt('Ready to go fishing?')
-
-if (likeFishing == 'yes'){
-    document.write('<h3>You have your priorities right</h3>');
-    document.write('<img src="https://content.osgnetworks.tv/gameandfishing/content/photos/gaf-12Punch-SMB.jpg"/>')
-}
