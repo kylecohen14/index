1cef922a1db1786d7a98ef2706b8b70ab25ee137
kylec <KYLECOHEN14@GMAIL.COM>
2021-05-26 21:08:04 -0400

    attempt at adding java

---
+++ app.js
@@ -0,0 +1,23 @@
+// let userName = 'Kyle'
+// userName = 'Summer'
+// console.log(userName)
+
+// userName = "Spring"
+// console.log(username)
+
+
+
+let userName = prompt('Enter Name: ');
+console.log(userName);
+document.write('<h2>' + userName + '</h2>');
+
+
+
+let userName = prompt('Do you love fishing?');
+console.log(userAnswer);
+
+if (userAnswer == 'yes') {
+    document.write('<h3>Meet me at the boat ramp! ' + userName +'</h3>');
+} else {
+    document.write('<h3>' + userName +', go buy some fishing gear.</h3>');
+}
---
+++ app2.js
@@ -0,0 +1,6 @@
+let likeFishing = prompt('Ready to go fishing?')
+
+if (likeFishing == 'yes'){
+    document.write('<h3>You have your priorities right</h3>');
+    document.write('<img src="https://content.osgnetworks.tv/gameandfishing/content/photos/gaf-12Punch-SMB.jpg"/>')
+}
