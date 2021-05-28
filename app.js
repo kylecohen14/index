2d41dd6df35ee5b8111c2393bfa43a70ebf3ad97
kylec <KYLECOHEN14@GMAIL.COM>
2021-05-27 21:33:18 -0400

    guessing game addition

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// let userName = 'Kyle'
+
 // userName = 'Summer'
 // console.log(userName)
 
@@ -17,7 +17,7 @@ function getUserName(){
 // let user1 = getUserName
 // console.log(user1);
 
-function getUserAnwser(){
+function getUserAnswer(){
     let question1 = prompt('Do you love fishing? (yes or no)');
 console.log(question1);
 
@@ -27,7 +27,7 @@ if (question1.toLowerCase() == 'yes') {
     document.write('<h3>Lets go buy some fishing gear.</h3>');
 }
 }
-function getUserAnwser(){
+function getUserAnswer(){
     let question2 = prompt('have you been fishing?')
     let url ="https://pbs.twimg.com/profile_images/605189676476039168/5qZTHZK__400x400.png"
     if (question2.toLowerCase() == 'yes'){
@@ -35,7 +35,45 @@ function getUserAnwser(){
         document.write('<img src="https://pbs.twimg.com/profile_images/605189676476039168/5qZTHZK__400x400.png"/>');
     }
 }
-let user1 = getUserAnwser();
+function userGuessingGame() {
+    correctAnswer = Math.floor(Math.random() * 1000) + 1;
+    console.log(correctAnswer)
+    userAttempts = 10;
+    
+    for(let i = 0; i <userAttempts; i++){
+        let userGuess = prompt("Pick a number between 1 - 1000");
+    while (userGuess < 1 || userGuess > 1000) {
+       userGuess = prompt('You are not following instructions. Please Enter a Number 1 - 1000');
+    }
+    if (userGuess == correctAnswer){
+        alert('Correct guess.');
+        break;
+    } else if(userGuess < correctAnswer){
+        alert('To low.')
+    } else if (userGuess > correctAnswer) {
+        alert('To high.')
+    } else {
+        alert('You did something wrong.')
+    }
+    console.log(i);
+    if (i == userAttempts - 1){
+        alert('You could not guess the answer. It is: ' + correctAnswer);
+    }
+    
+    }
+}
+
+userGuessingGame();
+
+function pikeTeeth(){
+    let numberofpikeTeeth = prompt('how many pike do you want to catch?')
+    for(let i = 0; i < numberofpikeTeeth; i ++){
+        document.write('<img src="' + url + '">')
+    }
+}
+
+
+
 
 let likeFishing = prompt('Ready to go fishing?');
 
